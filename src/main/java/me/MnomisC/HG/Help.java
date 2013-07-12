2f2cdc0aaab4a187ee025fda7845882677aa8ad3
MnomisC <scdockweiler@gmail.com>
2013-07-12 21:15:21 +0200

    * Fixed help

--- src/main/java/me/MnomisC/HG/Help.java
+++ src/main/java/me/MnomisC/HG/Help.java
@@ -7,15 +7,12 @@ public class Help {
 	String[] adminHelp = { "/hg random - Is awesome", "/hg LOL - dasjjsadj" };
 	int pages = 1;
 
-	public void admin(CommandSender sender, int page) {
+	public void help(CommandSender sender) {
 
-		if (page < pages) {
-			
-			sender.sendMessage("----------[Help - Page " + page + " of " + pages + "]");
-			for (int i = 0; i < adminHelp.length; i++) {
+		sender.sendMessage("----------[Help]----------");
+		for (int i = 0; i < adminHelp.length; i++) {
 
-				sender.sendMessage(adminHelp[i]);
-			}
+			sender.sendMessage(adminHelp[i]);
 		}
 	}
 }
--- src/main/java/me/MnomisC/HG/HungerGames.java
+++ src/main/java/me/MnomisC/HG/HungerGames.java
@@ -4,7 +4,6 @@ import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -26,26 +25,18 @@ public class HungerGames extends JavaPlugin {
 
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.logger.info(pdfFile.getName() + pdfFile.getName() + "has been ebabled");
-
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
 		if (commandLabel.equalsIgnoreCase("hungergames") || commandLabel.equalsIgnoreCase("hg")) {
-			if (!(args.length == 0)) {
+			if (args.length != 0) {
 				if (args[0].equalsIgnoreCase("help")) {
-					if (sender.hasPermission("hungergames.admin") || sender.isOp()
-					        || sender instanceof ConsoleCommandSender) {
-						if (args.length == 2) {
-							help.admin(sender, Integer.parseInt(args[1]));
-						} else {
-							help.admin(sender, 1);
-						}
-					}
+					help.help(sender);
 				}
 			} else {
-				help.admin(sender, 1);
+				help.help(sender);
 			}
 		}
 
