08f341406debced21ec2409696d115d0b26b4065
MnomisC <scdockweiler@gmail.com>
2013-07-12 19:08:19 +0200

    Just changed some stuff

--- src/main/java/me/MnomisC/HG/Help.java
+++ src/main/java/me/MnomisC/HG/Help.java
@@ -4,15 +4,15 @@ import org.bukkit.command.CommandSender;
 
 public class Help {
 
-	String[] adminHelp = {"/hg random - Is awesome", "/hg LOL - dasjjsadj"};
-	
+	String[] adminHelp = { "/hg random - Is awesome", "/hg LOL - dasjjsadj" };
+
 	public void admin(CommandSender sender, int page) {
-		
+
 		if (page < adminHelp.length) {
-			
-			sender.sendMessage("----------[Help - Page "+page+" of "+adminHelp.length+"]");
-			for (int i = 0; i<10;i++) {
-				
+
+			sender.sendMessage("----------[Help - Page " + page + " of " + adminHelp.length + "]");
+			for (int i = 0; i < 10; i++) {
+
 				sender.sendMessage(adminHelp[i]);
 			}
 		}
